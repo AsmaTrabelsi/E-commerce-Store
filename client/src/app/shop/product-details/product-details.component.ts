8e6c03f4300f54c83a3039e63ffb614891918ffc
asma <asmatra149@gmail.com>
2023-06-20 14:28:25 +0100

    add breadcrumb for navigation hierachy & spinner using interceptors

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
-
+import {BreadcrumbService} from 'xng-breadcrumb'
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,7 +11,10 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
 
   product?: Product;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDeatils','')
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -20,7 +23,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
    if(id) this.shopService.getProduct(+id).subscribe({
-    next: product => this.product= product,
+    next: product => {
+      this.product= product;
+      this.bcService.set('@productDeatils',product.name);
+    },
     error: error => console.log(error)
    });
   }
